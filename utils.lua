e48c3115a0e7ef6d85a88f2c8fc6be2a4738336d
DJ <57573747+exist3nce@users.noreply.github.com>
2021-07-03 14:17:54 -0700

    Create utils.lua

---
+++ utils.lua
@@ -0,0 +1,12 @@
+local utils = {}
+function utils.tfind(haystack, needle)
+	for i, v in pairs(haystack) do
+		if v == needle then
+			return true
+		else
+			if type(v) == "table" then
+				return utils.tfind(v, needle)
+			end
+		end
+	end
+end
